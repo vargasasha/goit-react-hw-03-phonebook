d5db5a6dbb9e1464b7ac91c2e0dc582e02053394
Oleksandr Varga <vargasasha@gmail.com>
2023-08-28 22:48:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,35 +3,54 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const localStorageKey = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  onAddContact = newContact => {
+  componentDidMount() {
+    const storageContacts = localStorage.getItem(localStorageKey);
 
+    if (storageContacts !== null) {
+      const savedContacts = JSON.parse(localStorage.getItem(localStorageKey));
+
+      this.setState({ contacts: savedContacts });
+
+      localStorage.setItem('contacts', JSON.stringify(initialContacts));
+
+    } else {
+      this.setState({ contacts: initialContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  onAddContact = newContact => {
     if (
       this.state.contacts.some(
         contact =>
-          contact.name.toLocaleLowerCase() === newContact.name.toLocaleLowerCase()
+          contact.name.toLocaleLowerCase() ===
+          newContact.name.toLocaleLowerCase()
       )
     ) {
       alert(`${newContact.name} is already in contacts`);
-    }
-    else {
+    } else {
       this.setState(prevState => {
         return {
           contacts: [...prevState.contacts, newContact],
         };
       });
     }
-    
   };
 
   onFilterName = filterName => {
